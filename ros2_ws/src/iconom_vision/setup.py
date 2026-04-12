7212acb32d8181b599218e9c5efb65ed4646906f
Ahmet Yusuf Şirin <65160352+ayusufsirin@users.noreply.github.com>
2026-04-13 01:17:29 +0300

    chore: add entry points for pose bridge utilities

--- ros2_ws/src/iconom_vision/setup.py
+++ ros2_ws/src/iconom_vision/setup.py
@@ -20,6 +20,9 @@ setup(
         "console_scripts": [
             "image_subscriber = iconom_vision.image_subscriber:main",
             "camera_symbology_overlay = iconom_vision.camera_symbology_overlay:main",
+            "gazebo_pose_publisher = iconom_vision.gazebo_pose_publisher:main",
+            "world_state_bridge = iconom_vision.world_state_bridge:main",
+            "pose_array_extractor = iconom_vision.pose_array_extractor:main",
         ],
     },
 )
